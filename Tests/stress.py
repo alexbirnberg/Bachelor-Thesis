d22907448900e75ec467d566223caf7da475046d
birnbergalex <76245883+birnbergalex@users.noreply.github.com>
2023-06-30 22:34:03 +0300

    More tests

--- Tests/stress.py
+++ Tests/stress.py
@@ -2,8 +2,8 @@
 import time
 import socket
 import struct
-import random
 import argparse
+import threading
 
 FILECACHE_CMD_READ  = 0
 FILECACHE_CMD_LOAD  = 1
@@ -13,6 +13,16 @@ SIZE                = 1024
 PORT                = 6666
 RAND                = 0xdeadbeef
 
+def stress():
+  message = struct.pack('<HIQIII', FILECACHE_CMD_READ, KEY, POS, SIZE, RAND, 0)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.sendto(message, (args.ip, PORT))
+      s.close()
+    except:
+      pass
+
 def main(args):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -20,14 +30,12 @@ def main(args):
   message = struct.pack('<HIQII', FILECACHE_CMD_LOAD, KEY, POS, SIZE, RAND)
   s.sendto(message, (args.ip, PORT))
   s.recvfrom(4)
-
-  # Read file contents
-  message = struct.pack('<HIQIII', FILECACHE_CMD_READ, KEY, POS, SIZE, RAND, 0)
-  start = time.perf_counter_ns()
-  s.sendto(message, (args.ip, PORT))
-  s.recvfrom(4)
-  print(str((time.perf_counter_ns() - start) / 1_000_000) + ' ms')
   s.close()
+
+  for i in range(100):
+    thread = threading.Thread(target=stress)
+    thread.start()
+  thread.join()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Perform latency test")
